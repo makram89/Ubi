4e9aa75021b89db1a59840d0ace9124fa96e79b9
makram89 <34419130+makram89@users.noreply.github.com>
2020-04-03 20:16:31 +0200

    step1_fin

--- FullMoonCounter/app/src/main/java/com/ubi/fullmooncounter/ui/currentMoonState/CurrentMoonStateFragment.kt
+++ FullMoonCounter/app/src/main/java/com/ubi/fullmooncounter/ui/currentMoonState/CurrentMoonStateFragment.kt
@@ -1,12 +1,14 @@
 package com.ubi.fullmooncounter.ui.currentMoonState
 
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.lifecycle.ViewModelProvider
 
 import com.ubi.fullmooncounter.R
@@ -51,6 +53,14 @@ class CurrentMoonStateFragment : Fragment() {
         full_moon_progress.text = "$percent%"
         last_new_moon.text = currentDate.minusDays(moonPhase.toLong()).toString()
         next_full_moon.text = currentDate.plusDays(daysTillFull.toLong()).toString()
+        //TODO check north or south
+        //setting image
+        val picName = "n$moonPhase"
+        context?.resources?.getIdentifier(picName,"drawable", context?.packageName)?.let {
+            moon_pic.setImageResource(
+                it
+            )
+        }
     }
 
 

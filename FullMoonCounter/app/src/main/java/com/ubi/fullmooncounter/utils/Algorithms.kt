cd0633d018af602f3176e4f45b76f4c02ad05a0d
makram89 <34419130+makram89@users.noreply.github.com>
2020-04-04 23:35:08 +0200

    algs_completed

--- FullMoonCounter/app/src/main/java/com/ubi/fullmooncounter/utils/Algorithms.kt
+++ FullMoonCounter/app/src/main/java/com/ubi/fullmooncounter/utils/Algorithms.kt
@@ -1,5 +1,7 @@
 package com.ubi.fullmooncounter.utils
 
+
+import java.util.*
 import kotlin.math.ceil
 import kotlin.math.floor
 import kotlin.math.sin
@@ -47,15 +49,62 @@ class Algorithms {
     }
 
     fun trig1(year: Int, month: Int, day: Int): Double {
-        return 0.0
+        val thisJD = julday(year, month, day)
+        val degToRad = 3.14159265 / 180
+        val K0 = floor((year - 1900) * 12.3685)
+        val T = (year - 1899.5) / 100
+        val T2 = T * T
+        var T3 = T * T * T
+        var J0 = 2415020 + 29 * K0
+        val F0 =
+            0.0001178 * T2 - 0.000000155 * T3 + (0.75933 + 0.53058868 * K0) - (0.000837 * T + 0.000335 * T2)
+        val M0 = 360 * (K0 * 0.08084821133)%1 + 359.2242 - 0.0000333 * T2 - 0.00000347 * T3
+        val M1 = 360 * (K0 * 0.07171366128)%1 + 306.0253 + 0.0107306 * T2 + 0.00001236 * T3
+        val B1 = 360 * (K0 * 0.08519585128)%1 + 21.2964 - 0.0016528 * T2 - 0.00000239 * T3
+        var oldJ = 0.0
+
+
+        var phase = 0.0
+        var jday = 0.0
+        while (jday < thisJD) {
+            var F = F0 + 1.530588 * phase
+            val M5 = (M0 + phase * 29.10535608) * degToRad
+            val M6 = (M1 + phase * 385.81691806) * degToRad
+            val B6 = (B1 + phase * 390.67050646) * degToRad
+            F -= 0.4068 * sin(M6) + (0.1734 - 0.000393 * T) * sin(M5)
+            F += 0.0161 * sin(2 * M6.toDouble()) + 0.0104 * sin(2 * B6.toDouble())
+            F -= 0.0074 * sin(M5 - M6.toDouble()) - 0.0051 * sin(M5 + M6.toDouble())
+            F += 0.0021 * sin(2 * M5.toDouble()) + 0.0010 * sin(2 * B6 - M6.toDouble())
+            F += 0.5 / 1440
+            oldJ = jday
+            jday = J0 + 28.0 * phase + floor(F)
+            phase++
+        }
+        return (thisJD - oldJ) % 30
     }
 
     fun conway(year: Int, month: Int, day: Int): Double {
-        return 0.0
+
+        var r = year % 100.0
+        r %= 19
+        if (r > 9) {
+            r -= 19
+        }
+        r = r * 11 % 30 + month + day
+        if (month < 3) {
+            r += 2
+        }
+        r -= if (year < 2000) 4.0 else 8.3
+        r = floor(r + 0.5) % 30
+        return if (r < 0) r + 30 else r
     }
 
     fun simple(year: Int, month: Int, day: Int): Double {
-        return 0.0
+        val lp = 2551443
+        val now = Date(year, month - 1, day, 20, 35, 0)
+        val newMoon = Date(1970, 0, 7, 20, 35, 0)
+        val phase = (now.time - newMoon.time) / 1000 % lp
+        return floor(phase / (24 * 3600).toDouble()) + 1
     }
 
 

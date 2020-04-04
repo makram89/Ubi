1bd0c98b7cb13f0ee22e1dc4cf99b54df9d258e7
makram89 <34419130+makram89@users.noreply.github.com>
2020-04-04 22:36:52 +0200

    more and more

--- FullMoonCounter/app/src/main/java/com/ubi/fullmooncounter/ui/currentMoonState/CurrentMoonStateViewModel.kt
+++ FullMoonCounter/app/src/main/java/com/ubi/fullmooncounter/ui/currentMoonState/CurrentMoonStateViewModel.kt
@@ -9,24 +9,10 @@ import kotlinx.coroutines.*
 
 class CurrentMoonStateViewModel(val algorithms: Algorithms) : ViewModel() {
 
-    val stateOfMoon = MutableLiveData<Double>()
-
-    private lateinit var job: Job
-
-//TODO choose algorithm
-
-    fun calculateState(year :Int, month : Int, day:Int) {
-        GlobalScope.launch {
-            stateOfMoon.value = algorithms.trig2(year, month, day)
-        }
-    }
-
     fun getMoonPhase(year :Int, month : Int, day:Int) : Double = when(MainActivity.algorithmName) {
         MainActivity.AlgorithmsNames.TRIG2 -> algorithms.trig2(year, month, day)
         MainActivity.AlgorithmsNames.TRIG1 -> algorithms.trig1(year, month, day)
         MainActivity.AlgorithmsNames.CONWAY -> algorithms.conway(year, month, day)
         MainActivity.AlgorithmsNames.SIMPLE -> algorithms.simple(year, month, day)
     }
-
-
 }
